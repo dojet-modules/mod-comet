a0b5252c7adba99da21a2ca34b997c1b7f04ec58
setimouse <setimouse@gmail.com>
2015-08-26 15:41:37 +0800

    init

---
+++ ModuleComet.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Filename: ModuleComet.class.php
+ *
+ * @author liyan
+ * @since 2015 8 26
+ */
+class ModuleComet extends BaseModule {
+
+}
---
+++ model/CometBaseAction.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * description
+ *
+ * Filename: CometBaseAction.class.php
+ *
+ * @author liyan
+ * @since 2015 8 26
+ */
+abstract class CometBaseAction extends XBaseAction {
+
+}
