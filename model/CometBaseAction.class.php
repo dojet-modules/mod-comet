ba2ee90be48a4eb78170ae2e1c077386ad28a098
setimouse <setimouse@gmail.com>
2015-08-26 16:29:24 +0800

    CometBaseAction

--- model/CometBaseAction.class.php
+++ model/CometBaseAction.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * description
+ * Comet技术服务端基类
  *
  * Filename: CometBaseAction.class.php
  *
@@ -9,4 +9,39 @@
  */
 abstract class CometBaseAction extends XBaseAction {
 
+    private $tickPerSecond = 10;
+    private $run = true;
+
+    final public function execute() {
+        $count = 0;
+        while ($this->run) {
+            $this->tick($count++);
+            if (!$this->run) {
+                break;
+            }
+            usleep($this->tickCycle());
+        }
+    }
+
+    final protected function flush() {
+        $this->run = false;
+        $this->output();
+    }
+
+    protected function setTickPerSecond($tickPerSecond) {
+        DAssert::assert($tickPerSecond > 0, 'illegal tick per second, should be number');
+        $this->tickPerSecond = $tickPerSecond;
+    }
+
+    protected function tickPerSecond() {
+        return $this->tickPerSecond;
+    }
+
+    protected function tickCycle() {
+        return 1000000 / $this->tickPerSecond;
+    }
+
+    abstract protected function tick($count);
+
+    abstract protected function output();
 }
